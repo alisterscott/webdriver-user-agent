3e325f7ff87f7862fd0b9d71943711d5b343c622
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-06-07 09:30:39 -0700

    refactored to move logic to Driver

--- lib/webdriver-user-agent.rb
+++ lib/webdriver-user-agent.rb
@@ -3,61 +3,25 @@ require 'facets/hash/except'
 require 'yaml'
 require 'json'
 require 'webdriver-user-agent/core_ext/symbol'
+require 'webdriver-user-agent/driver'
 
 module Webdriver
   module UserAgent
-    def self.driver options={} 
-      options[:browser] ||= :firefox
-      options[:agent] ||= :iphone
-      options[:orientation] ||= :portrait
-
-      user_agent_string = agent_string_for options[:agent]
-
-      case options[:browser]
-        when :firefox
-          options[:profile] ||= Selenium::WebDriver::Firefox::Profile.new
-          options[:profile]['general.useragent.override'] = user_agent_string
-        when :chrome
-          options[:switches] ||= []
-          options[:switches] << "--user-agent=#{user_agent_string}"
-        else
-          raise "WebDriver UserAgent currently only supports :firefox and :chrome."
-      end
-      driver = Selenium::WebDriver.for options[:browser], options.except(:browser, :agent, :orientation)
-      resize_inner_window(driver, *resolution_for(options[:agent], options[:orientation])) unless (options[:agent].downcase == :random)
-      driver
+    def self.driver options={}
+      Driver.instance.for options
     end
 
     def self.devices
-      @devices ||= YAML.load_file File.expand_path("../device-info/devices.yaml", __FILE__)
+      Driver.instance.devices
     end
 
     def self.resolution_for device_name, orientation
-      device = devices[device_name.downcase][orientation.downcase]
-      [device[:width],device[:height]]
+      Device.instance.resolution_for device_name, orientation
     end
 
     def self.agent_string_for device
-      user_agent_string = device.downcase == :random ? random_user_agent : devices[device.downcase][:user_agent]
-      raise "Unsupported user agent: '#{options[:agent]}'." unless user_agent_string
-      user_agent_string 
+      Driver.instance.agent_string_for device
     end 
-
-    private
-
-    def self.resize_inner_window driver, width, height
-      if driver.browser == :firefox or :chrome
-        driver.execute_script("window.open(#{driver.current_url.to_json},'_blank');")
-        driver.close
-        driver.switch_to.window driver.window_handles.first
-      end
-      driver.execute_script("window.innerWidth = #{width}; window.innerHeight = #{height};")
-    end
-
-    def self.random_user_agent
-      File.foreach(File.expand_path("../device-info/user_agents.txt", __FILE__)).each_with_index.reduce(nil) { |picked,pair| 
-      rand < 1.0/(1+pair[1]) ? pair[0] : picked }
-    end
 
   end
 end
---
+++ lib/webdriver-user-agent/driver.rb
@@ -0,0 +1,62 @@
+module Webdriver
+  module UserAgent
+    class Driver
+      include Singleton
+      
+      def for(options)
+        options[:browser] ||= :firefox
+        options[:agent] ||= :iphone
+        options[:orientation] ||= :portrait
+
+        user_agent_string = agent_string_for options[:agent]
+
+        case options[:browser]
+        when :firefox
+          options[:profile] ||= Selenium::WebDriver::Firefox::Profile.new
+          options[:profile]['general.useragent.override'] = user_agent_string
+        when :chrome
+          options[:switches] ||= []
+          options[:switches] << "--user-agent=#{user_agent_string}"
+        else
+          raise "WebDriver UserAgent currently only supports :firefox and :chrome."
+        end
+        driver = Selenium::WebDriver.for options[:browser], options.except(:browser, :agent, :orientation)
+        resize_inner_window(driver, *resolution_for(options[:agent], options[:orientation])) unless (options[:agent].downcase == :random)
+        driver
+      end
+
+      def devices
+        @devices ||= YAML.load_file File.expand_path("../../device-info/devices.yaml", __FILE__)
+      end
+
+
+      def resolution_for device_name, orientation
+        device = devices[device_name.downcase][orientation.downcase]
+        [device[:width],device[:height]]
+      end
+
+      def agent_string_for device
+        user_agent_string = device.downcase == :random ? random_user_agent : devices[device.downcase][:user_agent]
+        raise "Unsupported user agent: '#{options[:agent]}'." unless user_agent_string
+        user_agent_string 
+      end
+
+      private
+
+      def random_user_agent
+        File.foreach(File.expand_path("../../device-info/user_agents.txt", __FILE__)).each_with_index.reduce(nil) { |picked,pair| 
+          rand < 1.0/(1+pair[1]) ? pair[0] : picked }
+      end
+
+      def resize_inner_window driver, width, height
+        if driver.browser == :firefox or :chrome
+          driver.execute_script("window.open(#{driver.current_url.to_json},'_blank');")
+          driver.close
+          driver.switch_to.window driver.window_handles.first
+        end
+        driver.execute_script("window.innerWidth = #{width}; window.innerHeight = #{height};")
+      end
+
+    end
+  end
+end
