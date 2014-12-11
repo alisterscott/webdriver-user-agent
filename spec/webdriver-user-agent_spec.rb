75355314eb397b1581af3b19b4ee2d601be98d1e
Samuel Nissen <Samuel.Nissen@LinkShare.com>
2014-12-11 11:55:18 +0000

    Convert specs to RSpec 3.0.3 syntax with Transpec
    This conversion is done by Transpec 2.3.8 with the following command:
        transpec
    
    * 28 conversions
        from: obj.should
          to: expect(obj).to
    
    * 26 conversions
        from: == expected
          to: eq(expected)
    
    * 6 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/webdriver-user-agent_spec.rb
+++ spec/webdriver-user-agent_spec.rb
@@ -13,75 +13,75 @@ describe "webdriver user agent" do
 
   it "can create a new webdriver driver using firefox and iphone (portrait) by default" do
   	@driver = Webdriver::UserAgent.driver
-  	@driver.browser.should == :firefox
-  	@driver.execute_script('return navigator.userAgent').should include 'iPhone'
-  	@driver.execute_script('return window.innerWidth').should == 375 
-  	@driver.execute_script('return window.innerHeight').should == 559
+  	expect(@driver.browser).to eq(:firefox)
+  	expect(@driver.execute_script('return navigator.userAgent')).to include 'iPhone'
+  	expect(@driver.execute_script('return window.innerWidth')).to eq(375) 
+  	expect(@driver.execute_script('return window.innerHeight')).to eq(559)
   end
 
   it "can create a new webdriver driver using chrome and iphone 6 plus (landscape)" do
   	@driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :iphone6plus, :orientation => :landscape)
-  	@driver.browser.should == :chrome
-  	@driver.execute_script('return navigator.userAgent').should include 'iPhone'
-  	@driver.execute_script('return window.innerWidth').should == 736 
-  	@driver.execute_script('return window.innerHeight').should == 414
+  	expect(@driver.browser).to eq(:chrome)
+  	expect(@driver.execute_script('return navigator.userAgent')).to include 'iPhone'
+  	expect(@driver.execute_script('return window.innerWidth')).to eq(736) 
+  	expect(@driver.execute_script('return window.innerHeight')).to eq(414)
   end
 
   it "can create a new webdriver driver using chrome and iPad (landscape)" do
   	@driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :iPad, :orientation => :landscape)
-  	@driver.browser.should == :chrome
-  	@driver.execute_script('return navigator.userAgent').should include 'iPad'
-  	@driver.execute_script('return window.innerWidth').should == 1024 
-  	@driver.execute_script('return window.innerHeight').should == 690
+  	expect(@driver.browser).to eq(:chrome)
+  	expect(@driver.execute_script('return navigator.userAgent')).to include 'iPad'
+  	expect(@driver.execute_script('return window.innerWidth')).to eq(1024) 
+  	expect(@driver.execute_script('return window.innerHeight')).to eq(690)
   end
   
   it "can create a new webdriver driver using firefox and android phone (landscape)" do
     @driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :android_phone, :orientation => :landscape)
-    @driver.browser.should == :chrome
-    @driver.execute_script('return navigator.userAgent').should include 'Android'
-    @driver.execute_script('return window.innerWidth').should == 480 
-    @driver.execute_script('return window.innerHeight').should == 196
+    expect(@driver.browser).to eq(:chrome)
+    expect(@driver.execute_script('return navigator.userAgent')).to include 'Android'
+    expect(@driver.execute_script('return window.innerWidth')).to eq(480) 
+    expect(@driver.execute_script('return window.innerHeight')).to eq(196)
   end
 
   it "can create a new webdriver driver using firefox and android tablet (portrait)" do
     @driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :android_tablet, :orientation => :portrait)
-    @driver.browser.should == :chrome
-    @driver.execute_script('return navigator.userAgent').should include 'Android'
-    @driver.execute_script('return window.innerWidth').should == 768
-    @driver.execute_script('return window.innerHeight').should == 946
+    expect(@driver.browser).to eq(:chrome)
+    expect(@driver.execute_script('return navigator.userAgent')).to include 'Android'
+    expect(@driver.execute_script('return window.innerWidth')).to eq(768)
+    expect(@driver.execute_script('return window.innerHeight')).to eq(946)
   end
 
    it "can create a new webdriver driver using firefox and random user agent" do
     @driver = Webdriver::UserAgent.driver(:agent => :random)
-    @driver.browser.should == :firefox
-    @driver.execute_script('return navigator.userAgent').should_not be_nil
-    @driver.execute_script('return window.innerWidth').should_not == 320 
-    @driver.execute_script('return window.innerHeight').should_not == 356
+    expect(@driver.browser).to eq(:firefox)
+    expect(@driver.execute_script('return navigator.userAgent')).not_to be_nil
+    expect(@driver.execute_script('return window.innerWidth')).not_to eq(320) 
+    expect(@driver.execute_script('return window.innerHeight')).not_to eq(356)
   end
 
   it "can create a new webdriver driver using chrome and random user agent" do
     @driver = Webdriver::UserAgent.driver(:browser => :chrome, :agent => :random)
-    @driver.browser.should == :chrome
-    @driver.execute_script('return navigator.userAgent').should_not be_nil
-    @driver.execute_script('return window.innerWidth').should_not == 320 
-    @driver.execute_script('return window.innerHeight').should_not == 356
+    expect(@driver.browser).to eq(:chrome)
+    expect(@driver.execute_script('return navigator.userAgent')).not_to be_nil
+    expect(@driver.execute_script('return window.innerWidth')).not_to eq(320) 
+    expect(@driver.execute_script('return window.innerHeight')).not_to eq(356)
   end
 
   it "can create a new webdriver driver using an existing firefox profile" do
     profile = Selenium::WebDriver::Firefox::Profile.new
     profile['browser.startup.homepage'] = "data:text/html,<title>hello</title>"
     @driver = Webdriver::UserAgent.driver(:browser => :firefox, :profile => profile)
-    @driver.browser.should == :firefox
-    @driver.execute_script('return navigator.userAgent').should include 'iPhone'
-    @driver.execute_script('return window.innerWidth').should == 375 
-    @driver.execute_script('return window.innerHeight').should == 559
-    @driver.title.should == 'hello'
+    expect(@driver.browser).to eq(:firefox)
+    expect(@driver.execute_script('return navigator.userAgent')).to include 'iPhone'
+    expect(@driver.execute_script('return window.innerWidth')).to eq(375) 
+    expect(@driver.execute_script('return window.innerHeight')).to eq(559)
+    expect(@driver.title).to eq('hello')
   end
 
   it "can allow using selenium driver for watir browser" do
     @driver = Webdriver::UserAgent.driver(:browser => :firefox, :agent => :iphone, :orientation => :portrait)
     @browser = Watir::Browser.new @driver
-    @browser.url.should == "about:blank" 
+    expect(@browser.url).to eq("about:blank") 
   end
 
 
