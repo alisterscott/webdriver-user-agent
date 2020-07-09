9a53ce7a823de87341e88ae4c41905808412c362
Sam Nissen <samuel.nissen@rakuten.com>
2020-07-09 14:39:34 +0100

    Fix provided options, update devices & tests

--- lib/webdriver-user-agent.rb
+++ lib/webdriver-user-agent.rb
@@ -3,21 +3,22 @@ require 'webdriver-user-agent/driver'
 
 module Webdriver
   module UserAgent
-    def self.driver options={}
-      Driver.instance.for options
+    class << self
+      def driver(options = {})
+        Driver.instance.for options
+      end
+
+      def devices
+        Driver.instance.devices
+      end
+
+      def resolution_for(device_name, orientation)
+        Driver.instance.resolution_for device_name, orientation
+      end
+
+      def agent_string_for(device)
+        Driver.instance.agent_string_for device
+      end
     end
-
-    def self.devices
-      Driver.instance.devices
-    end
-
-    def self.resolution_for device_name, orientation
-      Driver.instance.resolution_for device_name, orientation
-    end
-
-    def self.agent_string_for device
-      Driver.instance.agent_string_for device
-    end 
-
   end
 end
